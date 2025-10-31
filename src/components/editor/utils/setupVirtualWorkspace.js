fe273a4ff19ef52465f6cf5d1891a8844a18a29f
anikitenko <alexvwan@gmail.com>
2025-10-31 11:10:46 +0200

    adjust condition

--- src/components/editor/utils/setupVirtualWorkspace.js
+++ src/components/editor/utils/setupVirtualWorkspace.js
@@ -44,15 +44,13 @@ export async function setupVirtualWorkspace(name, displayName, template, dir) {
         const sandboxName = "sandbox_" + name
         virtualFS.setInitWorkspace(name, sandboxName)
         const sandbox = localStorage.getItem(sandboxName)
-        if ((dir === "sandbox" || dir.includes(sandboxName)) || sandbox) {
-            if (sandbox) {
-                virtualFS.restoreSandbox(sandbox)
-            } else {
-                createVirtualFile(virtualFS.DEFAULT_FILE_MAIN, name, template)
-                createVirtualFile(virtualFS.DEFAULT_FILE_RENDER, name, template)
-                createVirtualFile("/package.json", packageJsonContent(name))
-                virtualFS.fs.create()
-            }
+        if (sandbox) {
+            virtualFS.restoreSandbox(sandbox)
+        } else if (dir === "sandbox" || dir.includes(sandboxName)) {
+            createVirtualFile(virtualFS.DEFAULT_FILE_MAIN, name, template)
+            createVirtualFile(virtualFS.DEFAULT_FILE_RENDER, name, template)
+            createVirtualFile("/package.json", packageJsonContent(name))
+            virtualFS.fs.create()
         } else {
             const data = await window.electron.plugin.getData(dir)
             if (data.success) {
